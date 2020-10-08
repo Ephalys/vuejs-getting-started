7a190fe2b1cfa0976853b3d1d7e8cd58a897f280
ephalys <ephalys@live.fr>
2020-10-08 16:17:22 +0200

    first commit

---
+++ src/views/Contact.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="more">
+    <Emoji altcode="💀"/>
+    <h1>This is a more page</h1>
+  </div>
+</template>
+<script>
+import Emoji from "@/components/Emoji";
+export default {
+  components: {Emoji}
+}
+</script>
